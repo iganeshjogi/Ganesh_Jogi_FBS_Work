2a785fc316956aa57933cf5ce9d2d21804b22d19
Ganesh Jogi <ganeshjogi2004@gmail.com>
2026-06-11 10:31:03 +0530

    Add files via upload

---
+++ Core Python/Assignments/Assignment4/q11.py
@@ -0,0 +1,22 @@
+# 11. WAP to check if given number Strong Number.
+
+n = int(input('Enter the number: '))
+
+temp = n
+total_sum = 0
+while n > 0:
+    d = n % 10
+
+    i = 1
+    fact = 1
+    while i <= d:
+        fact = fact * i
+        i += 1
+    
+    total_sum += fact 
+    n= n // 10
+
+if total_sum == temp:
+    print(f'The {temp} is a strong number.')
+else:
+    print(f'The {temp} is not a strong number.')
