a3f1520a4d55ad0f79603af4c939c31f0477726b
Ganesh Jogi <ganeshjogi2004@gmail.com>
2026-06-12 22:15:44 +0530

    Delete Core Python/Assignments/Assignment7 directory

--- Core Python/Assignments/Assignment7/q1h.py
+++
@@ -1,9 +0,0 @@
-for i in range(1,6):
-    for j in range(1,i+1):
-        print(j,end=' ')
-    for s in range(1,10-2*i):
-        print(' ',end=' ')
-    for k in range(i,0,-1):
-        if k != 5 or i !=5:
-            print(k,end=' ')
-    print()
