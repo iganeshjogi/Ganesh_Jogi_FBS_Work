15b6c3ed17a941abb574ab1134749b6a2cc9edc1
Ganesh Jogi <ganeshjogi2004@gmail.com>
2026-06-12 22:15:26 +0530

    Delete Core Python/Assignments/Assignment6 directory

--- Core Python/Assignments/Assignment6/q1e.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,6):
-    for s in range(5-i):
-        print(' ',end=' ')
-    for j in range(1,2*i):
-        print('*',end=' ')
-    print()
--- Core Python/Assignments/Assignment6/q1f.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,6):
-    for s in range(1,6-i):
-        print(' ',end=' ')
-    for j in range(1,2*i):
-        print(j,end=' ')
-    print()      
