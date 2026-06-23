b4cbe5c0ca450e2f2c4305dc34858ace8d69306f
Ganesh Jogi <ganeshjogi2004@gmail.com>
2026-06-23 22:09:17 +0530

    Add files via upload

---
+++ Core Python/Learning/Basics/addition.py
@@ -0,0 +1,9 @@
+num1 = int(input('Enter number 1:'))
+num2 = int(input('Enter number 2:'))
+
+sum = num1 + num2
+
+# print(sum)
+# print('Addition:',sum)
+# print('Addition:'+str(sum))
+print(f'Addition of {num1} and {num2} is {sum}.')
