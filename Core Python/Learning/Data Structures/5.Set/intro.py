a407145caf6bbc41fe94a3316a1d54638a939381
Ganesh Jogi <ganeshjogi2004@gmail.com>
2026-07-16 23:20:11 +0530

    Add files via upload

---
+++ Core Python/Learning/Data Structures/5.Set/intro.py
@@ -0,0 +1,18 @@
+# 1. {} with only values.
+
+s = set()
+
+s = {10, 20, 30, 'abc', (3.14, 20)}
+
+# 2. Hetrogeneous
+
+# 3. Unorded
+
+# 4. Mutable, Not editable
+
+# 5. Unique elements
+
+s.add(40)
+print(s)
+ 
+print(type(s))
---
+++ Core Python/Learning/Data Structures/5.Set/methods.py
@@ -0,0 +1,24 @@
+s1 = {10, 20, 30, 40}
+s2 = {30, 40, 50, 60}
+s3 = {50, 60}
+s4 = {70, 80}
+ 
+# s1.add(100)
+# s1.clear()
+# s5 = s1.copy()
+# print(s1.difference(s2))
+# s1.difference_update(s2)
+# s1.discard(30)
+# s1.remove(30)
+# print(s1.intersection(s2))
+# s1.intersection_update(s2)
+# print(s1.isdisjoint(s4))
+# print(s3.issubset(s2))
+# print(s2.issuperset(s3))
+# s1.pop()
+# print(s1.symmetric_difference(s2))
+# s1.symmetric_difference_update(s2)
+# print(s1.union(s2))
+# s1.update([50,60])
+
+print(s1)
