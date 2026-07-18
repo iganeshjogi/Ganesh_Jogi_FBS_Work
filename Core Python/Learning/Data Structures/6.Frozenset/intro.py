a46012971eaed06066918f4d2f02f6a712f6af38
Ganesh Jogi <ganeshjogi2004@gmail.com>
2026-07-18 16:35:49 +0530

    Add files via upload

---
+++ Core Python/Learning/Data Structures/6.Frozenset/intro.py
@@ -0,0 +1,14 @@
+# 1. Structure :  frozenset()
+
+fs = frozenset([10, 20, 30, 40, 40])
+
+# 2. Hetrogeneous
+
+# 3. Unorderd
+
+# 4. Immutable
+
+# 5. Unique elemets
+
+print(fs)
+print(type(fs)) 
---
+++ Core Python/Learning/Data Structures/6.Frozenset/methods.py
@@ -0,0 +1,12 @@
+fs = frozenset([10, 20, 30, 40])
+fs2 = frozenset([30, 40, 50, 60])
+
+# fs1 = fs.copy()
+# print(fs.difference(fs2))
+# print(fs.intersection(fs2))
+# print(fs.isdisjoint(fs2))
+# print(fs.issubset(fs2))
+# print(fs.issuperset(fs2))
+# print(fs.symmetric_difference(fs2))
+# print(fs.union(fs2))
+ 
