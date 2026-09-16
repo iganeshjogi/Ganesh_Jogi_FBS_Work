8a13ecda670e88e32f72ecde976f72528a938013
Ganesh Jogi <ganeshjogi2004@gmail.com>
2026-09-16 18:51:16 +0530

    Modules & Packages

---
+++ Core Python/Learning/Modules & Packages/myModule.py
@@ -0,0 +1,13 @@
+def chkEven(num):
+    if (num % 2 == 0):
+        return True
+    else:
+        return False
+    
+def chkPositive(num):
+    return num > 0
+
+print(__name__)
+
+# if( __name__ == '__main__'):
+#     print('This is my module file.')
